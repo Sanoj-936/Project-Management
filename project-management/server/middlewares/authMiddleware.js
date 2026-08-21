98fa28ba98623e98508c1f0d2dcf253c23e504e8
Sanoj-936 <sanoj93056@gmail.com>
2026-08-21 18:44:21 +0530

    Update project files

--- project-management/server/middlewares/authMiddleware.js
+++ project-management/server/middlewares/authMiddleware.js
@@ -1,13 +1,22 @@
+ import { verifyToken } from '@clerk/backend';
 
 export const protect = async (req, res, next) => {
     try {
-
-        const { userId } = await req.auth()
+        const authData = await req.auth();
+        const token = req.headers.authorization?.split(' ')[1];
+        if (token) {
+            try {
+                await verifyToken(token, { secretKey: process.env.CLERK_SECRET_KEY });
+            } catch (err) {
+                console.log("Token Verification Error:", err);
+            }
+        }
+        const { userId } = authData;
 
         if (!userId) {
             return res.status(401).json({ message: "Unauthorized" });
         }
-
+    
         return next();
     } catch (error) {
         console.log(error);
